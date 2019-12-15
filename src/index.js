893706ab98b965b5e57351d7984eca9354d8b3c2
Haiyan Sun <haiyansun@HaiyandeMacBook-Pro.local>
2019-12-15 16:38:28 +0800

    初步完成接口设计

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const dbUrl = process.env.DB_URL;
+console.log(dbUrl)
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    // useUnifiedTopol: true
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('db connect success!')
+}).catch(err => {
+    console.log(err)
+});
+
+const termsRouter = require('./routes/terms')
+
+app.use('/terms', termsRouter)
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${process.env.PORT}`)
+});
